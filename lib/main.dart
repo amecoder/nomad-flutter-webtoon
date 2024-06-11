ac6ee83d2f98156bd8b7c0093df6da69a7c1419a
munhyeongsu <moonhou@gentlemonster.com>
2024-06-11 17:10:39 +0900

    add stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,165 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+  List<int> numbers = [];
+
+  void onClick() {
+    setState(() {
+      numbers.add(numbers.length);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyLargeTitle(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    // always call once before build
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    // always call once before the widget is removed
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF181818),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text('Hey, Selena',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w800,
-                          )),
-                      Text(
-                        'welcome back',
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 0.8),
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 120,
-              ),
-              Text(
-                'Total Balance',
-                style: TextStyle(
-                  color: Color.fromRGBO(255, 255, 255, 0.8),
-                  fontSize: 14,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                '\$5 194 382',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                Button(
-                  text: 'Transter',
-                  bgColor: Color(0xFFF1B33B),
-                  textColor: Colors.black,
-                ),
-                Button(
-                  text: 'Request',
-                  bgColor: Color(0xFF1F2123),
-                  textColor: Colors.white,
-                ),
-              ]),
-              SizedBox(
-                height: 100,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Wallets',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 48,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    'View All',
-                    style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 0.8),
-                      fontSize: 18,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CurrencyCard(
-                name: 'Euro',
-                code: 'EUR',
-                amount: '6 428',
-                icon: Icons.euro_rounded,
-                isInverted: false,
-                order: 1,
-              ),
-              CurrencyCard(
-                name: 'Bitcoin',
-                code: 'BTC',
-                amount: '9 785',
-                icon: Icons.currency_bitcoin_rounded,
-                isInverted: true,
-                order: 2,
-              ),
-              CurrencyCard(
-                name: 'Dollar',
-                code: 'USD',
-                amount: '428',
-                icon: Icons.attach_money_rounded,
-                order: 3,
-                isInverted: false,
-              ),
-            ],
-          ),
-        ),
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
